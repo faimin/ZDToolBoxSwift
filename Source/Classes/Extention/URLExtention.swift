95ba706aed90edc54ce102a65e7b524878030f61
faimin <fuxianchao@gmail.com>
2020-11-18 18:47:11 +0800

    url Extention

---
+++ Source/Classes/Extention/URLExtention.swift
@@ -0,0 +1,17 @@
+//
+//  URLExtention.swift
+//  ZDSwiftToolKit
+//
+//  Created by Zero.D.Saber on 2020/11/18.
+//
+
+import Foundation
+
+//MARK: - URL Extension
+
+/// example: `let url: URL = "www.google.com"`
+extension URL: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self = URL(string: value)!
+    }
+}
