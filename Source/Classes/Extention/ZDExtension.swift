5fcd61723ad383c317487fc1bc5759de38d76dc3
faimin <fuxianchao@gmail.com>
2020-12-21 15:07:43 +0800

    class extension namespace

--- Source/Classes/Extention/ZDExtension.swift
+++ Source/Classes/Extention/ZDExtension.swift
@@ -19,16 +19,31 @@ public struct ZDSWraper<Base> {
     }
 }
 
-public protocol ZDObject: AnyObject { }
+public protocol ZDObject: AnyObject {
+    
+    associatedtype ZDObjectType
+    
+    /// 类变量
+    static var zd: ZDSWraper<ZDObjectType>.Type { get set }
+    
+    /// 实例变量
+    var zd: ZDSWraper<ZDObjectType> { get set }
+}
 
 extension ZDObject {
-    var zd: ZDSWraper<Self> {
+    public var zd: ZDSWraper<Self> {
         get {
             return ZDSWraper(self)
         }
-        set {
-            
-        }
+        set { }
     }
     
+    public static var zd: ZDSWraper<Self>.Type {
+        get {
+            return ZDSWraper<Self>.self
+        }
+        set { }
+    }
 }
+
+extension NSObject: ZDObject {}
