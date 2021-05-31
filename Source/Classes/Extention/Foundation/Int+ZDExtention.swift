54ef50c87333ab17afc25a24c8f23e4cce523761
faimin <fuxianchao@gmail.com>
2021-05-31 14:58:39 +0800

    Int extension

---
+++ Source/Classes/Extention/Foundation/Int+ZDExtention.swift
@@ -0,0 +1,14 @@
+//
+//  Int+ZDExtention.swift
+//  ZDSwiftToolKit
+//
+//  Created by Zero.D.Saber on 2021/5/31.
+//
+
+
+/// Bool -> Int
+extension Int: ExpressibleByBooleanLiteral {
+    public init(booleanLiteral value: BooleanLiteralType) {
+        self.init(value ? 1 : 0)
+    }
+}
