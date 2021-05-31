1fdf80273ef7a72fa2abb14b580dc607ce9057f3
faimin <fuxianchao@gmail.com>
2021-05-31 15:05:29 +0800

    Cell+ZDExtension

---
+++ Source/Classes/Extention/UI/Cell+ZDExtension.swift
@@ -0,0 +1,38 @@
+//
+//  Cell+ZDExtension.swift
+//  ZDSwiftToolKit
+//
+//  Created by Zero.D.Saber on 2021/5/31.
+//
+
+import UIKit
+
+public protocol ZDSCellProtocol: AnyObject {
+    
+    associatedtype T
+    
+    /// cell重用id
+    static func cellIdentifier() -> String
+    
+    /// 绑定数据
+    func bindModel(_ model: T)
+}
+
+public extension ZDSCellProtocol {
+    
+    static func cellIdentifier() -> String {
+        return "\(Self.self)"
+    }
+    
+    func bindModel(_ model: T) {
+        print("ZDSCellProtocol -> \(#function) 默认实现")
+    }
+}
+
+extension UITableViewCell: ZDSCellProtocol {
+    public typealias T = Any
+}
+
+extension UICollectionViewCell: ZDSCellProtocol {
+    public typealias T = Any
+}
