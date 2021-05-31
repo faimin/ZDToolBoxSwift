cccc8b1850ebe929f605d6045ce5cad56349697b
faimin <fuxianchao@gmail.com>
2021-05-31 14:55:45 +0800

    Collection extension

---
+++ Source/Classes/Extention/Foundation/Collection+ZDExtention.swift
@@ -0,0 +1,26 @@
+//
+//  Collection+ZDExtention.swift
+//  ZDSwiftToolKit
+//
+//  Created by Zero.D.Saber on 2021/5/31.
+//
+
+public extension Collection {
+    
+    /// 安全取值
+    /// - Parameters index: 下标
+    /// - Notes 来源：https://github.com/Luur/SwiftTips
+    subscript(mms index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+    
+    /// 安全取值并提供默认值
+    /// - Parameters
+    ///     - index: 下标
+    ///     - defaultValue: 默认值
+    /// - Notes
+    subscript(mms index: Index, defaultValue: Element) -> Element {
+        return indices.contains(index) ? self[index] : defaultValue
+    }
+}
+
