f474d0a21cf43c7c33570d897fcf220306c08b32
faimin <fuxianchao@gmail.com>
2021-05-31 14:57:40 +0800

    Dictionary extension

---
+++ Source/Classes/Extention/Foundation/Dictionary+ZDExtention.swift
@@ -0,0 +1,26 @@
+//
+//  Dictionary+ZDExtention.swift
+//  ZDSwiftToolKit
+//
+//  Created by Zero.D.Saber on 2021/5/31.
+//
+
+
+public extension ZDSWraper where T == [AnyHashable: Any] {
+    
+    /// 字典合并
+    mutating func merge<S>(_ other: S)
+    where S: Sequence, S.Iterator.Element == (key: T.Key, value: T.Value) {
+        for (key, value) in other {
+            self.base[key] = value
+        }
+    }
+}
+
+public extension Dictionary {
+    
+    /// 从字典取值并设置非空的默认值
+    subscript(mms key: Key, defaultValue: Value) -> Value {
+        return self[key] ?? defaultValue
+    }
+}
