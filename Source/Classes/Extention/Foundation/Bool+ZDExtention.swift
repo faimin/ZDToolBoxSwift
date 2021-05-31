a723cf312079304088c0a999b845c523352adc23
faimin <fuxianchao@gmail.com>
2021-05-31 14:54:41 +0800

    Bool extension

---
+++ Source/Classes/Extention/Foundation/Bool+ZDExtention.swift
@@ -0,0 +1,14 @@
+//
+//  Bool+ZDExtention.swift
+//  ZDSwiftToolKit
+//
+//  Created by Zero.D.Saber on 2021/5/31.
+//
+
+/// Int -> Bool
+extension Bool: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: IntegerLiteralType) {
+        self.init(value != 0)
+    }
+}
+
