f4523656ee33ce3adfd4d8c2015fcb6993bb0da3
faimin <fuxianchao@gmail.com>
2022-09-27 18:18:14 +0800

    symbol

--- ZDSwiftToolKitDemo/ZDSwiftToolKitDemo/ViewController.swift
+++ ZDSwiftToolKitDemo/ZDSwiftToolKitDemo/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 import ZDSwiftToolKit
 import os
 
+/// 函数签名要一一对应，否则会转换失败 @convention(thin) 也不用能缺了
+typealias F = @convention(thin) (_ params: [String: Any]) -> [String: Any]?
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -19,8 +22,23 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-//        os_log("error", dso: rw.dso, log: rw.log, type: .fault)
-//        os_log(.fault, dso: rw.dso, log: rw.log, "aaaaa", "vvv")
+        /*
+        os_log("error", dso: rw.dso, log: rw.log, type: .fault)
+        os_log(.fault, dso: rw.dso, log: rw.log, "aaaaa", "vvv")
+        */
+        let symbol = FindSymbolAddress("ZD://login")
+        let f = unsafeBitCast(symbol, to: F.self)
+        let _ = f(["navi": "符现超"])
+        print(symbol ?? "-------")
     }
 }
 
+@_silgen_name("ZD://login")
+public func LoginRouterInterface(with params: [String: Any]) -> [String: Any]? {
+    guard let navi = params["navi"] else {
+       return nil
+    }
+    print(navi)
+    
+    return nil
+}
