aee486c77387673837707b03cb5dc417e6243ce4
faimin <fuxianchao@gmail.com>
2024-12-30 13:58:57 +0800

    ZDWeakBox

---
+++ Source/Classes/Tool/ZDWeakBox.swift
@@ -0,0 +1,18 @@
+//
+//  ZDWeakBox.swift
+//  Pods
+//
+//  Created by Zero_D_Saber on 2024/12/30.
+//
+
+public struct ZDWeakBox<T: AnyObject> {
+    // MARK: Properties
+    
+    public weak var object: T?
+    
+    // MARK: Lifecycle
+    
+    public init(object: T? = nil) {
+        self.object = object
+    }
+}
