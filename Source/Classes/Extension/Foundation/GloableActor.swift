0f48a5d3fd83351bd3a8628deb04c5f822e40eb6
faimin <fuxianchao@gmail.com>
2025-01-07 15:41:54 +0800

    execute directly if on current queue

--- Source/Classes/Extension/Foundation/GloableActor.swift
+++ Source/Classes/Extension/Foundation/GloableActor.swift
@@ -11,13 +11,13 @@
 @globalActor
 public actor ZDGloableActor: GlobalActor {
     // MARK: Static Properties
-    
+
     public static var shared = ZDGloableActor()
-    
+
     private static let excutor = ZDExecutor()
-    
+
     // MARK: Static Computed Properties
-    
+
     public static var sharedUnownedExecutor: UnownedSerialExecutor {
         excutor.asUnownedSerialExecutor()
     }
@@ -28,17 +28,26 @@ public actor ZDGloableActor: GlobalActor {
 @available(iOS 13.0, *)
 private final class ZDExecutor: SerialExecutor {
     // MARK: Static Properties
-    
-    private static let dispatcher = DispatchQueue(label: "com.zd.actor.serial", qos: .utility)
-    
+
+    private static let specificKey = DispatchSpecificKey<String>()
+    private static let dispatcher = {
+        let queue = DispatchQueue(label: "com.zd.actor.serial", qos: .utility)
+        queue.setSpecific(key: specificKey, value: "actorExecutor")
+        return queue
+    }()
+
     // MARK: Functions
-    
+
     func enqueue(_ job: UnownedJob) {
-        ZDExecutor.dispatcher.async {
-            job.runSynchronously(on: self.asUnownedSerialExecutor())
+        guard DispatchQueue.getSpecific(key: Self.specificKey) != nil else {
+            Self.dispatcher.async {
+                job.runSynchronously(on: self.asUnownedSerialExecutor())
+            }
+            return
         }
+        job.runSynchronously(on: asUnownedSerialExecutor())
     }
-    
+
     func asUnownedSerialExecutor() -> UnownedSerialExecutor {
         UnownedSerialExecutor(ordinary: self)
     }
