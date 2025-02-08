c06fde1b6d9cced3cd0d9a4047677b1e1d598c57
faimin <fuxianchao@gmail.com>
2025-02-08 11:59:05 +0800

    ZDGradientView

---
+++ Source/Classes/SubClass/ZDGradientView.swift
@@ -0,0 +1,20 @@
+//
+//  ZDGradientView.swift
+//  Pods
+//
+//  Created by Zero_D_Saber on 2025/2/8.
+//
+
+@objc
+public final class ZDGradientView: UIView {
+    @objc public var gradientLayer: CAGradientLayer {
+        guard let layer = layer as? CAGradientLayer else {
+            fatalError()
+        }
+        return layer
+    }
+
+    override public class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+}
