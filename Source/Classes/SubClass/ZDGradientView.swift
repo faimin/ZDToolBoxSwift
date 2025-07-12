f6210001b136ba6a92f9bb692adde39c5a96e0f5
faimin <fuxianchao@gmail.com>
2025-07-12 15:17:16 +0800

    update ZDGradientView func

--- Source/Classes/SubClass/ZDGradientView.swift
+++ Source/Classes/SubClass/ZDGradientView.swift
@@ -5,16 +5,45 @@
 //  Created by Zero_D_Saber on 2025/2/8.
 //
 
-@objc
-public final class ZDGradientView: UIView {
+public class ZDGradientView: UIView {
+    // MARK: Overridden Properties
+
+    override public class var layerClass: AnyClass {
+        return CAGradientLayer.self
+    }
+
+    // MARK: Properties
+
+    @objc public var colors: [UIColor]? {
+        didSet {
+            gradientLayer.colors = colors?.map(\.cgColor)
+        }
+    }
+
+    @objc public var startPoint: CGPoint = .init(x: 0, y: 0.5) {
+        didSet {
+            gradientLayer.startPoint = startPoint
+        }
+    }
+
+    @objc public var endPoint: CGPoint = .init(x: 1, y: 0.5) {
+        didSet {
+            gradientLayer.endPoint = endPoint
+        }
+    }
+
+    @objc public var locations: [NSNumber]? {
+        didSet {
+            gradientLayer.locations = locations
+        }
+    }
+
+    // MARK: Computed Properties
+
     @objc public var gradientLayer: CAGradientLayer {
         guard let layer = layer as? CAGradientLayer else {
             fatalError("impossible appear")
         }
         return layer
-    }
-
-    override public class var layerClass: AnyClass {
-        return CAGradientLayer.self
     }
 }
