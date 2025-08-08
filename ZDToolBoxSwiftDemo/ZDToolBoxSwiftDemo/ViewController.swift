b09df700360fc1c5165a97c183fdd2fb0759c99a
faimin <fuxianchao@gmail.com>
2025-08-08 11:54:33 +0800

    test UILayoutGuide private method that called when update frame

--- ZDToolBoxSwiftDemo/ZDToolBoxSwiftDemo/ViewController.swift
+++ ZDToolBoxSwiftDemo/ZDToolBoxSwiftDemo/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 import ZDToolBoxSwift
 
+class ZDLayoutGuide: UILayoutGuide {
+    override func _updateLayoutFrame(inOwningView arg1: Any!, fromEngine arg2: Any!) {
+        super._updateLayoutFrame(inOwningView: arg1, fromEngine: arg2)
+        
+        print("22222----> ", layoutFrame)
+    }
+}
+
+class ZDGradientLabel: UILabel {
+    override class var layerClass: AnyClass {
+        CAGradientLayer.self
+    }
+}
+
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let label = ZDGradientLabel()
+        let l = label.layer
+        print(l)
+        
         let _ = ZDExcuteSymbol()
+        
+        let layout = ZDLayoutGuide()
+        self.view.addLayoutGuide(layout)
+        //view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            layout.widthAnchor.constraint(equalTo: view.widthAnchor),
+            layout.heightAnchor.constraint(equalTo: view.heightAnchor),
+        ])
     }
 
     override func viewDidAppear(_ animated: Bool) {
