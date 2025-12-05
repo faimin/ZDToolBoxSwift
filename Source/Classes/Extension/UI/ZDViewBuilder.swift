b0b3da2d5df11a771d15a627b1d3f3f9e7f69370
faimin <fuxianchao@gmail.com>
2025-12-05 12:58:23 +0800

    easier to understand with typealias

--- Source/Classes/Extension/UI/ZDViewBuilder.swift
+++ Source/Classes/Extension/UI/ZDViewBuilder.swift
@@ -8,7 +8,14 @@
 // MARK: - ZDViewBuilder
 
 @resultBuilder
-public struct ZDViewBuilder<V> {
+public struct ZDViewBuilder<T> {
+    // MARK: Nested Types
+
+    public typealias Expression = T
+    public typealias Component = [T]
+
+    // MARK: Static Functions
+
     /// resultBuilder document:
     /// https://doc.swiftgg.team/documentation/the-swift-programming-language/attributes/#resultBuilder
     ///
@@ -16,41 +23,41 @@ public struct ZDViewBuilder<V> {
     ///    表达式 → buildExpression → 部分结果 → buildBlock → 最终结果
     ///
     /// `buildExpression` 输出 `[V]`，把所有变量转换成了数组，所以 `buildBlock` 期望的输入是 `[V]`
-    public static func buildExpression(_ expression: V?) -> [V] {
+    public static func buildExpression(_ expression: Expression?) -> Component {
         guard let expression = expression else {
             return []
         }
         return [expression]
     }
 
-    public static func buildExpression(_ expression: V) -> [V] {
+    public static func buildExpression(_ expression: Expression) -> Component {
         [expression]
     }
 
-    public static func buildOptional(_ component: [V]?) -> [V] {
+    public static func buildOptional(_ component: Component?) -> Component {
         guard let component = component else {
             return []
         }
         return component
     }
 
-    public static func buildEither(first component: [V]) -> [V] {
+    public static func buildEither(first component: Component) -> Component {
         component
     }
 
-    public static func buildEither(second component: [V]) -> [V] {
+    public static func buildEither(second component: Component) -> Component {
         component
     }
 
-    public static func buildLimitedAvailability(_ component: [V]) -> [V] {
+    public static func buildLimitedAvailability(_ component: Component) -> Component {
         component
     }
 
-    public static func buildBlock(_ components: [V]...) -> [V] {
+    public static func buildBlock(_ components: Component...) -> Component {
         components.flatMap { $0 }
     }
 
-    public static func buildArray(_ components: [[V]]) -> [V] {
+    public static func buildArray(_ components: [Component]) -> Component {
         components.flatMap { $0 }
     }
 }
